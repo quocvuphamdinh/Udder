0542609b7a3c9d2f0e13624edf0d8c5fc1beae4a
vupham <quocvuphamdinh>
2024-07-04 20:26:03 +0700

    Initial Commit

---
+++ Udder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Udder
+//
+//  Created by Pham Dinh Quoc Vu on 4/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Pham Dinh Quoc Vu on 4/7/24.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
